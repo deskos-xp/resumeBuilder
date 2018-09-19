d8b546cc833920afc6f226af1f4e8608d5731ddc
k.j.hirner.wisdom@gmail.com <k.j.hirner.wisdom@gmail.com>
2018-09-18 19:32:44 -0700

    updates

--- ubuntu-install-deps.sh
+++ ubuntu-install-deps.sh
@@ -3,6 +3,7 @@
 function main(){
 	packages=('python3-pyqt5' 'python3-lxml' 'ttf-mscorefonts-installer' 'python3-pip' 'python3-setuptools' 'python3-lxml')
 	sudo apt-get install $packages
+	sudo ln -s /bin/bash /usr/bin/bash
 	pip3 install reportlab
 
 }
